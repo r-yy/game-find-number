94d9f0368e0d0ec7cb1bb6f6b8a0d67d406240d1
r-yy <104170759+r-yy@users.noreply.github.com>
2022-04-22 13:47:43 +0300

    Add files via upload

---
+++ main.swift
@@ -0,0 +1,34 @@
+import Foundation
+print("Компьютер загадал число от 1 до 250, тебе нужно его угадать!")
+var randNumber = UInt8.random(in: 1...250)
+var userNumber: String?
+loop: while true {
+    print("Введите число:")
+    userNumber = readLine()
+    if let x = UInt8(userNumber!) {
+        if x > randNumber {
+            print("Загаданное число меньше!")
+        } else if x < randNumber {
+            print("Загаданное число больше!")
+        } else {
+            print("Угадал! Было загадано число - \(randNumber) \nХотите сыграть снова? (Да/Нет)")
+        loopOne: while true {
+                let newGame = readLine()
+                switch newGame {
+                case "Да", "да":
+                    randNumber = UInt8.random(in: 1...250)
+                    continue loop
+                case "Нет", "нет":
+                    print("Игра окончена, приходи играть снова!")
+                    break loop
+                default:
+                    print("Введите (Да) или (Нет)")
+                    continue loopOne
+                }
+            }
+        }
+    } else {
+        print("Введите корректное число")
+    }
+}
+
